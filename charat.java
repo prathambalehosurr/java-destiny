f5e16fc6994c7ee41b749983d99c8a20574d1c93
prathambalehosurr <prathambalehosur.work@mail.com>
2026-07-22 10:35:39 +0530

    Update charat.java string and add index.java to demonstrate character indexing and retrieval

--- charat.java
+++ charat.java
@@ -1,6 +1,6 @@
 public class charat {
     public static void main(String[] args) {
-        String txt = "Find the character in this text";
+        String txt = "Hello, How are you?";
 
         System.out.println(txt.charAt(7));
     }
---
+++ index.java
@@ -0,0 +1,12 @@
+public class index {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        
+        int index = str.indexOf('W');
+        System.out.println("Index of 'W': " + index);
+
+        char ch = str.charAt(7);
+        System.out.println("Character at index 7: " + ch);
+
+    }
+}
