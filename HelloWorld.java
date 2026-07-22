1d49c8dc0ba0b4e953e36e34b21a32e9777a6712
prathambalehosurr <prathambalehosur.work@mail.com>
2026-07-22 09:27:43 +0530

    Add multiple Java classes demonstrating basic programming concepts

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Enter your Name: Pratham Balehosur\n");
+        System.out.println("Grade: A");
+        System.out.println("Percentage: 88");
+    }
+}
