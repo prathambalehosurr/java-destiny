0a9cedeb744d49b1e4283e8b5364afd066ae510e
prathambalehosurr <prathambalehosur.work@mail.com>
2026-07-22 09:40:05 +0530

    Add itterate.java to demonstrate array iteration in Java

---
+++ itterate.java
@@ -0,0 +1,8 @@
+public class itterate {
+    public static void main(String[] args) {
+        int nums[] = {10, 20, 30};
+            for (int i = 0; i < nums.length; i++) {
+                System.out.print(nums[i] + " ");
+        }
+    }
+}
