440fb8d197028fab7dce69f239b2e1c267c2a52d
prathambalehosurr <prathambalehosur.work@mail.com>
2026-07-22 09:43:14 +0530

    Update itterate.java to modify array values and enhance iteration logic

--- itterate.java
+++ itterate.java
@@ -1,8 +1,9 @@
-public class itterate {
-    public static void main(String[] args) {
-        int nums[] = {10, 20, 30};
-            for (int i = 0; i < nums.length; i++) {
-                System.out.print(nums[i] + " ");
+    public class itterate {
+        public static void main(String[] args) {
+            int num[] = {25,50,75,100};
+            for (int i=0;i<4;i++){
+                System.out.println(num[i]);
+            
+            }
         }
     }
-}
