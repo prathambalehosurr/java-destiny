aa008102f181fee7a50cd5c3719bd1f91846de28
prathambalehosurr <prathambalehosur.work@mail.com>
2026-07-22 09:46:10 +0530

    Add multidimention.java to demonstrate multidimensional array usage in Java

---
+++ multidimention.java
@@ -0,0 +1,10 @@
+public class multidimention {
+    public static void main(String[] args) {
+        int num[][] = {{25,50,75,100},{125,150,175,200}};
+        for (int i=0;i<2;i++){
+            for (int j=0;j<4;j++){
+                System.out.println(num[i][j]);
+            }
+        }
+    }
+}
