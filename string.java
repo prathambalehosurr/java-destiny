b1244c3c00cfc9314825942888a0b36844437c7d
prathambalehosurr <prathambalehosur.work@mail.com>
2026-07-22 10:42:35 +0530

    Remove unnecessary string object instantiation in string.java

--- string.java
+++ string.java
@@ -1,6 +1,5 @@
 public class string {
     public static void main(String[] args) {
-        string a = new string();
         String str = "Hello, World!";
         int len = str.length();
         System.out.println(" String: " + str);
